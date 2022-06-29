30959d7f3e7b0137b4d70599afd6653b795a2081
Alexander Chasovnikov <a.s.chasovnikov@yandex.ru>
2022-06-29 17:25:04 +0300

    create views: main/index, admin/main/index and others

---
+++ resources/views/main/index.blade.php
@@ -0,0 +1,153 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div id="main-wrapper" style="position: relative; overflow: visible; box-sizing: border-box; min-height: 1px;">
+
+    <div class="theiaStickySidebar" style="padding-top: 0px; padding-bottom: 1px; position: static; transform: none; top: 0px; left: 471.5px;">
+        <div class="main section" id="main" name="Main Posts">
+            <div class="widget Blog" data-version="2" id="Blog1">
+                <div class="blog-posts hfeed container index-post-wrap">
+                    <div class="grid-posts">
+                        <div class="blog-post hentry index-post post-0">
+                            <div class="post-image-wrap">
+                                <a class="post-image-link" href="https://nitro-soratemplates.blogspot.com/2018/05/how-good-team-can-positively-influence_23.html">
+                                    <img alt="How a good team can positively influence your business" class="post-thumb" src="https://1.bp.blogspot.com/-LRRkXVe_sfk/Xtoiou_T2DI/AAAAAAAACfs/FN3QMOYzsGYP45RVuhXagJaVFeq9lCehACLcBGAsYHQ/w72-h72-p-k-no-nu/TY%2BImage%2B%25281%2529.webp">
+                                </a>
+                            </div>
+                            <div class="post-info">
+                                <h2 class="post-title">
+                                    <a href="https://nitro-soratemplates.blogspot.com/2018/05/how-good-team-can-positively-influence_23.html">How a good team can positively influence your business</a>
+                                </h2>
+                                <div class="post-meta">
+                                    <span class="post-tag index-post-tag">
+                                        Business
+                                    </span><i>-</i>
+                                    <span class="post-date published" datetime="2018-05-23T20:17:00-07:00">May 23, 2018</span>
+                                </div>
+                                <a class="read-more" href="https://nitro-soratemplates.blogspot.com/2018/05/how-good-team-can-positively-influence_23.html">Read more</a>
+                            </div>
+                        </div>
+                        <div class="blog-post hentry index-post post-1">
+                            <div class="post-image-wrap">
+                                <a class="post-image-link" href="https://nitro-soratemplates.blogspot.com/2018/05/successful-analysis-can-become-key-to_23.html">
+                                    <img alt="Successful analysis can become the key to your business success" class="post-thumb" src="https://1.bp.blogspot.com/-cHZ_ZtrRfRA/Xtoi0uZZJLI/AAAAAAAACfw/qNiA0yHdj8I7bsvRCYjJqalxkKm1BJW-gCLcBGAsYHQ/w72-h72-p-k-no-nu/TY%2BImage%2B%25282%2529.webp">
+                                </a>
+                            </div>
+                            <div class="post-info">
+                                <h2 class="post-title">
+                                    <a href="https://nitro-soratemplates.blogspot.com/2018/05/successful-analysis-can-become-key-to_23.html">Successful analysis can become the key to your business success</a>
+                                </h2>
+                                <div class="post-meta">
+                                    <span class="post-tag index-post-tag">
+                                        Business
+                                    </span><i>-</i>
+                                    <span class="post-date published" datetime="2018-05-23T20:13:00-07:00">May 23, 2018</span>
+                                </div>
+                                <a class="read-more" href="https://nitro-soratemplates.blogspot.com/2018/05/successful-analysis-can-become-key-to_23.html">Read more</a>
+                            </div>
+                        </div>
+                        <div class="blog-post hentry index-post post-2">
+                            <div class="post-image-wrap">
+                                <a class="post-image-link" href="https://nitro-soratemplates.blogspot.com/2018/05/good-partnerships-can-help-your-company_23.html">
+                                    <img alt="Good partnerships can help your company achieve better results" class="post-thumb" src="https://1.bp.blogspot.com/-4X-om85llo4/Xtoi6jdURTI/AAAAAAAACf4/jId6tefXvLwY1kPGjj6x3mgeb_qyYZDqQCLcBGAsYHQ/w72-h72-p-k-no-nu/TY%2BImage%2B%25283%2529.webp">
+                                </a>
+                            </div>
+                            <div class="post-info">
+                                <h2 class="post-title">
+                                    <a href="https://nitro-soratemplates.blogspot.com/2018/05/good-partnerships-can-help-your-company_23.html">Good partnerships can help your company achieve better results</a>
+                                </h2>
+                                <div class="post-meta">
+                                    <span class="post-tag index-post-tag">
+                                        Business
+                                    </span><i>-</i>
+                                    <span class="post-date published" datetime="2018-05-23T20:06:00-07:00">May 23, 2018</span>
+                                </div>
+                                <a class="read-more" href="https://nitro-soratemplates.blogspot.com/2018/05/good-partnerships-can-help-your-company_23.html">Read more</a>
+                            </div>
+                        </div>
+                        <div class="blog-post hentry index-post post-3">
+                            <div class="post-image-wrap">
+                                <a class="post-image-link" href="https://nitro-soratemplates.blogspot.com/2018/05/methods-that-can-help-you-use-your_23.html">
+                                    <img alt="Methods that can help you use your credit card more safely" class="post-thumb" src="https://1.bp.blogspot.com/-Ybc48bbjLwI/XtojAoJv7mI/AAAAAAAACf8/tQoV4G-Y3yYkTKBhazyHiWvCQomZoZv8QCLcBGAsYHQ/w72-h72-p-k-no-nu/TY%2BImage%2B%25284%2529.webp">
+                                </a>
+                            </div>
+                            <div class="post-info">
+                                <h2 class="post-title">
+                                    <a href="https://nitro-soratemplates.blogspot.com/2018/05/methods-that-can-help-you-use-your_23.html">Methods that can help you use your credit card more safely</a>
+                                </h2>
+                                <div class="post-meta">
+                                    <span class="post-tag index-post-tag">
+                                        Business
+                                    </span><i>-</i>
+                                    <span class="post-date published" datetime="2018-05-23T20:03:00-07:00">May 23, 2018</span>
+                                </div>
+                                <a class="read-more" href="https://nitro-soratemplates.blogspot.com/2018/05/methods-that-can-help-you-use-your_23.html">Read more</a>
+                            </div>
+                        </div>
+                        <div class="blog-post hentry index-post post-4">
+                            <div class="post-image-wrap">
+                                <a class="post-image-link" href="https://nitro-soratemplates.blogspot.com/2018/05/see-some-alternatives-to-bitcoin-that_23.html">
+                                    <img alt="See some alternatives to bitcoin that can be a good investment" class="post-thumb" src="https://1.bp.blogspot.com/-TlSvL1JDKz4/XtojS4amw5I/AAAAAAAACgM/pNfFYI-wzxo1oTP9fVosx54WWkkiXFmgQCLcBGAsYHQ/w72-h72-p-k-no-nu/TY%2BImage%2B%25285%2529.webp">
+                                </a>
+                            </div>
+                            <div class="post-info">
+                                <h2 class="post-title">
+                                    <a href="https://nitro-soratemplates.blogspot.com/2018/05/see-some-alternatives-to-bitcoin-that_23.html">See some alternatives to bitcoin that can be a good investment</a>
+                                </h2>
+                                <div class="post-meta">
+                                    <span class="post-tag index-post-tag">
+                                        Bitcoin
+                                    </span><i>-</i>
+                                    <span class="post-date published" datetime="2018-05-23T20:01:00-07:00">May 23, 2018</span>
+                                </div>
+                                <a class="read-more" href="https://nitro-soratemplates.blogspot.com/2018/05/see-some-alternatives-to-bitcoin-that_23.html">Read more</a>
+                            </div>
+                        </div>
+                        <div class="blog-post hentry index-post post-5">
+                            <div class="post-image-wrap">
+                                <a class="post-image-link" href="https://nitro-soratemplates.blogspot.com/2018/05/know-right-time-to-acquire-bitcoins_23.html">
+                                    <img alt="Know the right time to acquire bitcoins with lower price" class="post-thumb" src="https://1.bp.blogspot.com/-K6pzSaa4Pfw/XtojaTgVcHI/AAAAAAAACgQ/GCwAevkjpWk1kb_rAnVGkltFHr47amqHgCLcBGAsYHQ/w72-h72-p-k-no-nu/TY%2BImage%2B%25286%2529.webp">
+                                </a>
+                            </div>
+                            <div class="post-info">
+                                <h2 class="post-title">
+                                    <a href="https://nitro-soratemplates.blogspot.com/2018/05/know-right-time-to-acquire-bitcoins_23.html">Know the right time to acquire bitcoins with lower price</a>
+                                </h2>
+                                <div class="post-meta">
+                                    <span class="post-tag index-post-tag">
+                                        Bitcoin
+                                    </span><i>-</i>
+                                    <span class="post-date published" datetime="2018-05-23T19:59:00-07:00">May 23, 2018</span>
+                                </div>
+                                <a class="read-more" href="https://nitro-soratemplates.blogspot.com/2018/05/know-right-time-to-acquire-bitcoins_23.html">Read more</a>
+                            </div>
+                        </div>
+                        <div class="blog-post hentry index-post post-6">
+                            <div class="post-image-wrap">
+                                <a class="post-image-link" href="https://nitro-soratemplates.blogspot.com/2018/05/saving-bitcoins-can-be-good-investment_23.html">
+                                    <img alt="Saving Bitcoins can be a good investment for the future" class="post-thumb" src="https://1.bp.blogspot.com/-vdj3KyHIACg/XtojgxYeAaI/AAAAAAAACgU/p14Mn32RYZYI-1_Z-t-GcWDpvLOTWOptQCLcBGAsYHQ/w72-h72-p-k-no-nu/TY%2BImage%2B%25287%2529.webp">
+                                </a>
+                            </div>
+                            <div class="post-info">
+                                <h2 class="post-title">
+                                    <a href="https://nitro-soratemplates.blogspot.com/2018/05/saving-bitcoins-can-be-good-investment_23.html">Saving Bitcoins can be a good investment for the future</a>
+                                </h2>
+                                <div class="post-meta">
+                                    <span class="post-tag index-post-tag">
+                                        Bitcoin
+                                    </span><i>-</i>
+                                    <span class="post-date published" datetime="2018-05-23T19:58:00-07:00">May 23, 2018</span>
+                                </div>
+                                <a class="read-more" href="https://nitro-soratemplates.blogspot.com/2018/05/saving-bitcoins-can-be-good-investment_23.html">Read more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="blog-pager container" id="blog-pager"><span class="page-of">Page 1 of 11</span><span class="page-num page-active">1</span><a class="page-num" href="#" onclick="getPage(2);return false">2</a><a class="page-num" href="#" onclick="getPage(3);return false">3</a><span class="page-num page-dots">...</span><a class="page-num" href="#" onclick="getPage(11);return false">11</a><a class="page-num page-next" href="#" onclick="getPage(2);return false"></a></div>
+
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Main\IndexController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', IndexController::class);
+
+Route::group(['namespace' => '\App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController');
+    });
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
