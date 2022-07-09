4b44f6cae4d0ea81a829864075ff9d9482353c9b
Alexander Chasovnikov <a.s.chasovnikov@yandex.ru>
2022-07-09 16:14:45 +0300

    install telescope; query optimization

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,6 +14,7 @@ class Post extends Model
     protected $table = 'posts';
     protected $guarded = false;
     protected $withCount = ['likedUsers'];
+    protected $with = ['category'];
 
     public function tags()
     {
